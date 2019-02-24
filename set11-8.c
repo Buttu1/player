9590236e62e7f34bbb9c383ca41264280ea358f5
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2019-02-24 13:44:52 +0530

    Create set11-8.c

---
+++ set11-8.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+int a[20],i,n,temp=1,pos=0,k,min;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&n);
+}
+scanf("%d",&k);
+while(temp<=k)
+{
+min=a[0];
+for(i=0;i<n;i++)
+{
+if(a[i]<min)
+{
+min=a[i];
+pos=i;
+}
+}
+if(temp==k)
+{
+printf("%d",min);
+}
+else
+{
+a[pos]=0;
+}
+temp++;
+}
+getch();
+}
