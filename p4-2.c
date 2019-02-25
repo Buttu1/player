0f763cefae30591c1a47f70aff9457a3bff8fa97
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2019-02-25 10:26:07 +0530

    Create p4-2.c

---
+++ p4-2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int n,k;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+scanf("%d",&k);
+for(i=0;i<n;i++)
+{
+if(a[i]==k)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
