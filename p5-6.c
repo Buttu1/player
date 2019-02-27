5ba70c7177c76b6ec27ce7fa08be396f36079854
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2019-02-27 05:42:00 +0530

    Create p5-6.c

---
+++ p5-6.c
@@ -0,0 +1,19 @@
+include <stdio.h>
+void main()
+{
+   int A,B,C;
+   scanf("%d %d %d",&A,&B,&C);
+   if(A>0&&B>0&&C>0)
+   {
+       if(A+B+C==180)
+       printf("yes");
+       else
+       printf("no");
+       
+       
+       
+   }
+   else
+   printf("no");
+  
+}
