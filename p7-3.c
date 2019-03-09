a82640d535d35b39831c929965eaa2d9edfb6600
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2019-03-09 20:22:22 +0530

    Create p7-3.c

---
+++ p7-3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void main()
+{
+int N;
+scanf("%d",&N);
+int A[N],B[N],i,j,c=0;
+for(i=0;i<N;i++)
+{
+    scanf("%d",&A[i]);
+}
+
+for(i=0;i<N;i++)
+{
+    scanf("%d",&B[i]);
+}
+ 
+ 
+ for(i=0;i<N;i++)
+ {
+     for(j=0;j<N;j++)
+ { 
+    if(A[i]==B[j])
+     {
+     
+     break;
+         
+     }   
+ }
+ 
+ if(c!=1)
+ printf("%d ",A[i]);
+ }
+ 
