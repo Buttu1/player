22bb5f8443cfe9dff3a5c2c2926e059a59031623
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2019-03-10 14:28:58 +0530

    Create p10-10.c

---
+++ p10-10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+	int n,res,bin=0,t=1;
+	scanf("%d",&n);
+	while(n>=1)
+	{
+	    res=n%10;
+	    bin=res*t+bin;
+	    n=n/10;
+	    t=t*2;
+	}
+	printf("%x",bin);
+
+}
+
