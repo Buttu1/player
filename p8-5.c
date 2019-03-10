c1717142d18ac967430c836debf8ab72b76f6f06
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2019-03-10 10:42:55 +0530

    Rename p8-4.c to p8-5.c

---
+++ p8-5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<math.h>
+void main()
+{
+   int N,a[100000],i,j,c=0;
+   scanf("%d",&N);
+   for(i=0;i<N;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   
+      for(i=0;i<N;i++)
+   {
+       
+      for(j=i+1;j<N;j++)
+   {
+      if(a[i]<a[j])
+      c++;
+
+   
+       
+     
+   }
+   
+   }
+   printf("%d",c);
+}
