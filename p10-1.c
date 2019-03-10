7357c83a43130268f257723bfcf02c892642297d
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2019-03-10 13:50:21 +0530

    Create p10-1.c

---
+++ p10-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+  long N,r,base=1,bin=0;
+   scanf("%ld",&N);
+   
+   while(N)
+   {
+       
+       r=N%2;
+       N=N/2;
+       bin=bin+(r*base);
+      base=base*10;
+      
+   }
+   printf("%ld",bin);
+
+}
