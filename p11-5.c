3d06884757e253e3674719ae8a39c2d21789ccf5
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2019-03-11 16:03:40 +0530

    Create p11-5.c

---
+++ p11-5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int i,a[20],n,temp;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]<a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+for(i=0;i<n;i++)
+{
+printf("%d",i+1);
+}
+getch();
+}
